c1d7779935863b3aef9f8e1de829ad8583afe441
Chris White <chris.white@codingzeal.com>
2019-03-22 16:49:13 -0600

    Configure :nerves_network to use device wifi

--- config/config.exs
+++ config/config.exs
@@ -56,10 +56,20 @@ node_name = if Mix.env() != :prod, do: "cheese_monitor"
 
 config :nerves_init_gadget,
   ifname: "usb0",
-  address_method: :dhcpd,
   mdns_domain: "nerves.local",
   node_name: node_name,
-  node_host: :mdns_domain
+  node_host: :mdns_domain,
+  ifname: "wlan0",
+  address_method: :dhcp
+
+key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
+
+config :nerves_network, :default,
+  wlan0: [
+    ssid: System.get_env("NERVES_NETWORK_SSID"),
+    psk: System.get_env("NERVES_NETWORK_PSK"),
+    key_mgmt: String.to_atom(key_mgmt)
+  ]
 
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
